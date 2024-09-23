0b99e4f8b05d4f47a9f413c58eb3dc1add373542
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-09-23 10:15:49 -0500

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import UserNavbar from './components/UserNavbar/UserNavbar';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">AAA
+      <header>
+        <UserNavbar />
       </header>
     </div>
   );
