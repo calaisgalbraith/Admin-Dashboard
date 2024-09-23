fd48b17d7c3cbe4cf3b9031820b8200e8a9a6b15
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-09-23 15:33:58 -0500

    fixed navbar styling

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -2,7 +2,7 @@ import './Navbar.css';
 function Navbar() {
   return (
     <>
-        <nav className="navbar bg-dark" id="dashboardNavbar">
+        <nav className="navbar bg-dark fixed-top" id="dashboardNavbar">
         <div className="container-fluid">
             <a class="navbar-brand">Paws Shelter</a>
             <div className="d-flex">
