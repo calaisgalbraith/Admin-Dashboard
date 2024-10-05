44cc9c8b0ffc18aa6e454c3c27b9434f838f40b8
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-10-04 20:29:42 -0500

    added about container

---
+++ src/components/AboutContainer/AboutContainer.js
@@ -0,0 +1,11 @@
+import './AboutContainer.css'
+
+function AboutContainer() {
+  return (
+    <div id="aboutContainer">
+      About Info here!
+    </div>
+  );
+}
+
+export default AboutContainer;
