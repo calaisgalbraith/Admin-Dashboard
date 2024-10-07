0bbd6138ff71cc19615fe2f8c195fce3d53b6586
“calaisgalbraith” <calaisgalbraith@gmail.com>
2024-10-07 16:36:42 -0500

    removed link from navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,16 +1,15 @@
 import './Navbar.css';
 import { FaPaw } from "react-icons/fa";
-import { Link } from "react-router-dom";
 
-function Navbar() {
+function Navbar({ changeFunc }) {
   return (
     <header>
         <nav className="navbar bg-dark fixed-top" id="dashboardNavbar">
         <div className="container-fluid">
-          <Link to='/' className='navbar-brand'>
+          <div className='navbar-brand'>
               <FaPaw />
               <span>Paws Shelter</span>
-          </Link>
+          </div>
             <div className="d-flex">
                 Signed in as: System Admin
             </div>
